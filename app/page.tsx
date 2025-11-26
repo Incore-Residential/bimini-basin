b6e3b0606869bfc6fd15126e43ee7efaa6012deb
Incore-Residential <areyes@incoreresidential.com>
2025-11-26 11:49:04 -0500

    Add Footer component and integrate into Home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Amenities } from "@/components/Amenities";
 import { FloorPlans } from "@/components/FloorPlans";
 import { ThreeDTours } from "@/components/ThreeDTours";
 import { Contact } from "@/components/Contact";
+import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <FloorPlans />
       <ThreeDTours />
       <Contact />
+      <Footer />
     </main>
   );
 }
